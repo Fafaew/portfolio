a063a7204912089a7b2bc7460f8a02387b039d4b
fafaew <rafael.gomest@gmail.com>
2023-11-30 19:42:11 -0300

    Refact components structure and added variables

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Navbar.module.scss';
+
+const Navbar = () => {
+  return (
+    <div className={styles.teste}>Navbar</div>
+  )
+}
+
+export default Navbar
