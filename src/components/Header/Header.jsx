5a341b4e639785a8652bbb15698a18b223457bde
fafaew <rafael.gomest@gmail.com>
2023-12-11 16:11:04 -0300

    Added header with typing effect and contact button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import styles from './Header.module.scss'
+import styles from './Header.module.scss';
+import { TypeAnimation } from 'react-type-animation';
+
 
 const Header = () => {
   return (
-    <div className={styles.headerWraper}></div>
+    <div className={styles.headerWraper}>
+      <TypeAnimation
+      sequence={[
+        'Node',
+        1000,
+        'JavaScript',
+        1000,
+        'React',
+        1000,
+        'Shopify',
+        1000
+      ]}
+      className={styles.typingWrapper}
+      wrapper="span"
+      speed={30}
+      repeat={Infinity}
+    />
+    <div className={styles.contactWrapper}>
+      <a href="/" className={styles.contactButton}>
+        Contact me
+      </a>
+    </div>
+    </div>
   )
 }
 
