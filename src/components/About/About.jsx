d4d201afb94a4850429debadd449b5d56eeb31ef
fafaew <rafael.gomest@gmail.com>
2023-12-17 19:03:19 -0300

    about | added icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './About.module.scss';
 import picture from '../../utils/images/picture.jpg';
 import { FaNode, FaReact, FaShopify } from "react-icons/fa";
+import { FaBookOpenReader } from "react-icons/fa6";
 import { RiJavascriptFill } from "react-icons/ri";
+import { IoGameControllerOutline } from "react-icons/io5";
+import { GiWeightLiftingUp } from "react-icons/gi";
+
+
 
 
 
@@ -15,17 +20,25 @@ const About = () => {
 
       <div className={styles.aboutWrapper}>
         <h1 className={styles.aboutHeading}>about me</h1>
-        <p>
+        <p className={styles.aboutIntro}>
           Hello everyone! I'm Rafael Gomes, a web developer from Minas Gerais,
           Brazil. I have been developing websites for over 2 years.
         </p>
-        <p>Apart from coding, some other activities that I love to do:</p>
-        <div className={styles.aboutMarker}></div>
-        <p className={styles.aboutText}>Play Video Games</p>
-        <div className={styles.aboutMarker}></div>
-        <p className={styles.aboutText}>Practice Sports</p>
-        <div className={styles.aboutMarker}></div>
-        <p className={styles.aboutText}>Learn about financial market</p>
+        <br />
+        <p className={styles.aboutIntro}>Apart from coding, some other activities that I love to do:</p>
+        <br />
+        <div className={styles.aboutActivities}>
+          <IoGameControllerOutline />
+          <p className={styles.aboutText}>Play Video Games</p>
+        </div>
+        <div className={styles.aboutActivities}>
+          <GiWeightLiftingUp />
+          <p className={styles.aboutText}>Practice Sports</p>
+        </div>
+        <div className={styles.aboutActivities}>
+          <FaBookOpenReader />
+          <p className={styles.aboutText}>Learn about financial market</p>
+        </div>
         <br />
         <p>Technologies that I use:</p>
         <div className="aboutIcons">
